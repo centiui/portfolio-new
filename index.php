9a716e9aab7acf0d1bf046a7c23db26a6e1bb2a8
Centi <98369279+lovecenti@users.noreply.github.com>
2023-05-13 20:15:42 -0400

    parallax

--- index.php
+++ index.php
@@ -45,15 +45,28 @@
     </header>
     <main>
         <div class="hero">
-            <div class="container text-center">
-                <h1 class="hero-title">Custom websites, designs, and advertisements</h1>
-                <h2 class="hero-description">Get professional services tailored for your business</h2>
-                <div class="hero-btns mt-5">
-                    <a href="" class="btn btn-primary btn-lg">Get a Quote <span class="iconify" data-icon="material-symbols:keyboard-arrow-down" data-rotate="270deg"></span></a> <a href="" class="btn btn-secondary btn-lg">Services <span class="iconify" data-icon="material-symbols:keyboard-arrow-down"></span></a>
+            <div class="container hero-padding">
+                <div class="row">
+                    <div class="col-xl-8">
+                        <h1 class="hero-title">Custom websites, designs, and ads.</h1>
+                        <h2 class="hero-description">Tailored for your business.</h2>
+                        <div class="hero-btns mt-5">
+                            <a href="" class="btn btn-primary btn-lg">Get a Quote <span class="iconify" data-icon="material-symbols:keyboard-arrow-down" data-rotate="270deg"></span></a> <a href="" class="btn btn-secondary btn-lg">Services <span class="iconify" data-icon="material-symbols:keyboard-arrow-down"></span></a>
+                        </div>
+                    </div>
+                    <div class="col-xl-4" style="height: 500px;">
+                        
+                        <div class="wrapper">
+                            <div id="scene">
+                            <img data-depth="2.0" src="assets/img/atimg.png" class="hero-img" width="20%" alt="">
+                            <img data-depth="3.0" src="assets/img/compimg.png" class="hero-img compimg" width="100%" alt="">
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-        
+
     </main>
     <footer>
         <div class="loader-wrapper">
@@ -62,9 +75,14 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
         <script src="https://code.iconify.design/3/3.1.0/iconify.min.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/parallax/3.1.0/parallax.min.js" integrity="sha512-/6TZODGjYL7M8qb7P6SflJB/nTGE79ed1RfJk3dfm/Ib6JwCT4+tOfrrseEHhxkIhwG8jCl+io6eaiWLS/UX1w==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        <script>
+            var scene = document.getElementById('scene');
+            var parallax = new Parallax(scene);
+        </script>
         <script>
             window.addEventListener("scroll", function() {
-                var header = document.querySelector("headerauto");
+                var header = document.querySelector("nav");
                 header.classList.toggle("stickyauto", window.scrollY > 0);
             })
         </script>
@@ -90,6 +108,7 @@
             const hiddenElements = document.querySelectorAll('.hidden');
             hiddenElements.forEach((el) => observer.observe(el));
         </script>
+        
     </footer>
 </body>
 
